f47402cef8acc9dd5bec097c79f684555729d98c
Surya Prakash <suryasrinivas765@gmail.com>
2025-06-27 00:14:56 +0530

    vpc module

--- peering.tf
+++ peering.tf
@@ -20,3 +20,39 @@ resource "aws_vpc_peering_connection" "foo" {
     }
   )
 }
+
+# Route from roboshop VPC to Default VPC
+# FOr practice purpose we are creating route for all subnets
+resource "aws_route" "public_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id 
+  
+}
+
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id 
+  
+}
+
+resource "aws_route" "database_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id 
+  
+}
+
+# Route from default VPC to roboshp VPC
+
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = data.aws_route_table.main.id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id 
+  
+}
