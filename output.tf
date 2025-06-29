942beb9b5296d9576f137b0095c65e231d5aaffd
Surya Prakash <suryasrinivas765@gmail.com>
2025-06-30 00:22:14 +0530

    vpc module

--- output.tf
+++ output.tf
@@ -2,14 +2,14 @@ output "vpc_id" {
     value = aws_vpc.main.id
 }
 
-output "public_subnet_id" {
+output "public_subnet_ids" {
     value = aws.subneet.public[*].id  
 }
 
-output "private_subnet_id" {
+output "private_subnet_ids" {
     value = aws.subneet.private[*].id  
 }
 
-output "database_subnet_id" {
+output "database_subnet_ids" {
     value = aws.subneet.databasae[*].id  
 }
